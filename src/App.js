86e8239110fd6f4d9b56c054c4abb3f88ca5d415
nadabk <nadbak95@gmail.com>
2018-05-16 16:58:48 +0100

    changed my app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <Router>
+
         <div className="App">
+      
           <Link to='/contacts'>
             <input type='button' value='contact list' />
           </Link>
